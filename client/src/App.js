91409ba507aaf4ed658b0f48d734251946223682
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-08 15:27:43 -0500

    conditionally renders home or login

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+
+/* 
+Components:
+  home - if not logged in, admin link/button at top right
+          if logged in, logout button
+  loop through FAQs - render FAQ component (if not logged in, just view)(if logged in, edit/delete buttons, created & edited user emails)
+  login - conditionally render based on auth state (make false after success)
+
+  admin state - bool
+*/
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">HELLO WORLD</header>
-    </div>
+  const [loggingIn, setIsLoggingIn] = useState(false);
+  return loggingIn ? (
+    <Login setIsLoggingIn={setIsLoggingIn} />
+  ) : (
+    <Home setIsLoggingIn={setIsLoggingIn} />
   );
 }
 
