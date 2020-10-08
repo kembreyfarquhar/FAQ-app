55998d8a3879466fac1c0fcf088534176e0c5e8a
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-07 22:47:31 -0500

    creates faqs table migration

---
+++ Backend/database/migrations/20201007212848_create-faqs-table.js
@@ -0,0 +1,15 @@
+exports.up = function (knex) {
+    return knex.schema.createTable("faqs", tbl => {
+        tbl.increments();
+        tbl.string("question", 255).notNullable();
+        tbl.string("answer", 1000).notNullable();
+        tbl.integer("created_by").notNullable().references("id").inTable("admins").onUpdate("cascade").onDelete("cascade");
+        tbl.timestamp("created_at").defaultTo(knex.fn.now());
+        tbl.timestamp("updated_at").defaultTo(knex.fn.now());
+        tbl.integer("last_edited_by").notNullable().references("id").inTable("admins").onUpdate("cascade").onDelete("cascade");
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists("faqs");
+};
