767da7c0c50a07438bccd61080875e2aacf39217
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-08 11:40:30 -0500

    adds token restricted middleware

--- Backend/customMiddleware/adminValidator.js
+++ Backend/customMiddleware/adminValidator.js
@@ -1,12 +1,32 @@
+require("dotenv").config();
 const bcrypt = require("bcryptjs");
 const Admins = require("../api/controllers/adminsController");
-require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET || "secret";
 
 module.exports = {
   adminValidator,
   validateWithPassword,
   ipValidator,
+  tokenRestricted,
 };
+
+function tokenRestricted(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Invalid token" });
+      } else {
+        req.jwtToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "Please provide a token" });
+  }
+}
 
 function ipValidator(req, res, next) {
   let ip =
