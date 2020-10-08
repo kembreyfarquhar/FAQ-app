f3888fd5dbb8f9e3701ee1782819c60109b2188e
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-08 00:58:04 -0500

    creates admin controller to query the database

---
+++ Backend/api/controllers/adminsController.js
@@ -0,0 +1,20 @@
+const db = require("../../database/dbConfig");
+
+module.exports = {
+    add,
+    findById,
+    findByEmail
+};
+
+async function add(admin) {
+    const [newAdmin] = await db("admins").insert(admin).returning("*");
+    return newAdmin;
+}
+
+function findById(id) {
+    return db("admins").where({ id }).first();
+}
+
+function findByEmail(email) {
+    return db("admins").where({ email }).first();
+}
