e528e74553ba963eb9a6b0e114b9ad875c8da463
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-08 16:58:18 -0500

    creates css file for home styles

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,17 +4,26 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
+import "../styles/Home.css";
 
 export const Home = (props) => {
   return (
-    <Container style={{ border: "1px solid red", paddingTop: "15px" }}>
+    <Container id="home-container">
       <Row>
         <Col>
-          <Image src={logo} style={{ height: "100px", width: "100px" }} />
+          <Image src={logo} id="home-logo" />
         </Col>
         <Col style={{ textAlign: "right" }}>
-          <p onClick={() => props.setIsLoggingIn(true)}>Admin</p>
+          <p
+            id="login-logout-button"
+            onClick={() => props.setIsLoggingIn(true)}
+          >
+            {props.isAdmin ? "Logout" : "Admin"}
+          </p>
         </Col>
+      </Row>
+      <Row id="faq-heading-row">
+        <h1 id="faq-heading">Frequently Asked Questions</h1>
       </Row>
     </Container>
   );
