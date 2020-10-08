916e7e5f0261e542507bef18ecf89c848fb497f9
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-08 01:01:31 -0500

    connects authRouter to the server

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -3,6 +3,8 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 
+const authRouter = require("./routers/authRouter");
+
 const server = express();
 
 server.use(helmet());
@@ -10,10 +12,12 @@ server.use(express.json());
 server.use(cors());
 server.use(morgan("combined"));
 
+server.use("/api/auth", authRouter);
+
 server.get("/", (_req, res) => {
-  res.send(
-    '<h1>Welcome to the FAQ App API!</h1><h3>For documentation <a href="https://github.com/kembreyfarquhar/FAQ-app">click here</a>.</h3><p>This API was made by <a href="https://github.com/kembreyfarquhar">Katie Embrey-Farquhar</a> with ❤️</p>'
-  );
+    res.send(
+        '<h1>Welcome to the FAQ App API!</h1><h3>For documentation <a href="https://github.com/kembreyfarquhar/FAQ-app">click here</a>.</h3><p>This API was made by <a href="https://github.com/kembreyfarquhar">Katie Embrey-Farquhar</a> with ❤️</p>'
+    );
 });
 
 module.exports = server;
