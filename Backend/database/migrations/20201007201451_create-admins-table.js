f903f8efc77e164ef663ccd9b6e8aea127a43f40
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-07 22:47:01 -0500

    edits admins table, removes username

--- Backend/database/migrations/20201007201451_create-admins-table.js
+++ Backend/database/migrations/20201007201451_create-admins-table.js
@@ -1,9 +1,8 @@
 exports.up = function (knex) {
     return knex.schema.createTable("admins", tbl => {
         tbl.increments();
-        tbl.string("username", 25).notNullable().unique();
-        tbl.string("password", 255).notNullable();
         tbl.string("email", 255).notNullable().unique();
+        tbl.string("password", 255).notNullable();
     });
 };
 
