d6bfaa4f665eb89f5540ebb463c3171700518fdc
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-08 11:42:57 -0500

    adds post and get one faq endpoints

--- Backend/api/routers/faqsRouter.js
+++ Backend/api/routers/faqsRouter.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const FAQS = require("../controllers/faqsController");
+const validator = require("../../customMiddleware/faqValidator");
+const restricted = require("../../customMiddleware/adminValidator");
 
 //GET ALL FAQS
 router.get("/", (req, res) => {
@@ -10,6 +12,30 @@ router.get("/", (req, res) => {
     .catch((err) =>
       res.status(500).json({ error: err.toString(), detail: err.detail })
     );
+});
+
+//GET ONE FAQ
+router.get("/:id", validator.validateFaqId, (req, res) => {
+  res.status(202).json(req.faq);
+});
+
+//POST FAQ
+router.post("/", restricted.tokenRestricted, (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ error: "Please provide a question and answer." });
+  }
+
+  const userId = req.jwtToken.subject;
+  const faq = { ...req.body, created_by: userId, last_edited_by: userId };
+  const status = validator.faqValidator(faq);
+
+  if (!status.isSuccessful) {
+    res.status(400).json(status);
+  } else {
+    FAQS.add(faq).then((saved) => {
+      res.status(201).json(saved);
+    });
+  }
 });
 
 module.exports = router;
