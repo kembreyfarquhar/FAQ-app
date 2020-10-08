3db28ffce18a84213d3a0ca7c669bf1079e8a5ca
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-07 19:49:05 -0500

    local server running

---
+++ Backend/knexfile.js
@@ -0,0 +1,45 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 6,
+      createTimeoutMillis: 3000,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+    },
+    connection: {
+      database: process.env.DB_DEV_DATABASE,
+      user: process.env.DB_DEV_USER,
+      password: process.env.DB_DEV_PASSWORD
+    },
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  },
+
+  production: {
+    client: "pg",
+
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    useNullAsDefault: true,
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  }
+};
