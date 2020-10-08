193968a77880cb64d5033b4ff1a3c5a91152de96
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-08 00:59:43 -0500

    creates password validation for admin login

---
+++ Backend/customMiddleware/validator.js
@@ -0,0 +1,30 @@
+const bcrypt = require("bcryptjs");
+const Admins = require("../api/controllers/adminsController");
+
+module.exports = {
+    validateWithPassword
+}
+
+async function validateWithPassword(req, res, next) {
+    const { email, password } = req.body;
+
+    if (!password || !email) {
+        res.status(400).json({ error: "Please provide a password and email" });
+    }
+
+    try {
+        const admin = await Admins.findByEmail(email);
+        console.log(bcrypt.compareSync(password, admin.password))
+
+        if (admin && bcrypt.compareSync(password, admin.password)) {
+            req.admin = admin;
+            next();
+        } else {
+            res.status(401).json({ error: "Invalid credentials" });
+        }
+    } catch (err) {
+        res
+            .status(500)
+            .json({ error: err.toString(), message: "something went wrong" });
+    }
+}
