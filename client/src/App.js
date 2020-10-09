ef1c9f27257dcb7b851dc36843230a69bcb37e5e
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-09 14:20:08 -0500

    passes props, adds useEffect checking for token

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Home } from "./components/Home";
 import { Login } from "./components/Login";
 
 function App() {
   const [loggingIn, setIsLoggingIn] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(true);
+
+  useEffect(() => {
+    if (localStorage.getItem("faq_token")) {
+      setIsAdmin(true);
+    }
+  }, [isAdmin]);
 
   return loggingIn ? (
-    <Login setIsLoggingIn={setIsLoggingIn} isAdmin={isAdmin} />
+    <Login setIsLoggingIn={setIsLoggingIn} setIsAdmin={setIsAdmin} />
   ) : (
-    <Home setIsLoggingIn={setIsLoggingIn} setIsAdmin={setIsAdmin} />
+    <Home
+      setIsLoggingIn={setIsLoggingIn}
+      isAdmin={isAdmin}
+      setIsAdmin={setIsAdmin}
+    />
   );
 }
 
