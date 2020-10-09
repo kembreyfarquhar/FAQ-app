48ba33e74e4f2b94074f4c774e919b9bebaa1b12
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-09 10:50:28 -0500

    removes notes

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,6 @@ import React, { useState } from "react";
 import { Home } from "./components/Home";
 import { Login } from "./components/Login";
 
-/* 
-Components:
-  home - if not logged in, admin link/button at top right
-          if logged in, logout button
-  loop through FAQs - render FAQ component (if not logged in, just view)(if logged in, edit/delete buttons, created & edited user emails)
-  login - conditionally render based on auth state (make false after success)
-
-  admin state - bool
-*/
-
 function App() {
   const [loggingIn, setIsLoggingIn] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
