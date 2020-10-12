a2bed9dd4081c20ffb81a560ceab3f01ac32dcef
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-12 04:01:17 -0500

    removes unused useEffect

--- client/src/components/Faq.js
+++ client/src/components/Faq.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../styles/Faq.css";
 import axios from "axios";
 import Card from "react-bootstrap/Card";
