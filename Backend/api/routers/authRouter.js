265d07ba83a7898de3a831c9ec403345f7027868
Katie Embrey-Farquhar <kembrey@prologuetechnology.com>
2020-10-12 16:44:47 -0500

    adds forgot password email sender

--- Backend/api/routers/authRouter.js
+++ Backend/api/routers/authRouter.js
@@ -4,6 +4,7 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET || "secret";
+const nodemailer = require("nodemailer");
 
 const Admins = require("../controllers/adminsController");
 const adminValidator = require("../../customMiddleware/adminValidator");
@@ -50,6 +51,76 @@ router.post("/login", adminValidator.validateWithPassword, (req, res) => {
 router.post("/:token", adminValidator.tokenRestricted, (req, res) => {
   res.status(200).json(req.token);
 });
+
+//FORGOT PASSWORD
+router.post(
+  "/email/:email",
+  adminValidator.validateAdminEmail,
+  async (req, res) => {
+    const email = req.params.email;
+    let password = makePassword(10);
+
+    const mailOptions = {
+      from: process.env.YAHOO_USER,
+      to: email,
+      subject: "FAQ App Password Change",
+      html: `<h3>You've requested a password change.</h3><br><p>Here is your temporary password: <strong>${password}</strong></p><br><p>Please visit the site <a href="http://localhost:3000/">here</a> and sign in with this password. Once you're done, please change it to something more personal and secure.</p><br><br><p>Thank you,</p><p>Curate FAQ App Team</p>`,
+    };
+
+    try {
+      const hash = bcrypt.hashSync(password, 10);
+      password = hash;
+      const updatedUser = await Admins.updatePassword(email, password);
+      wrappedSendMail(mailOptions)
+        .then(() => {
+          res.status(200).json({ message: "Password updated, email sent" });
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ error: err.toString(), message: "Something went wrong" });
+    }
+  }
+);
+
+async function wrappedSendMail(mailOptions) {
+  return new Promise((resolve, reject) => {
+    const transporter = nodemailer.createTransport({
+      host: "stmp.mail.yahoo.com",
+      port: 465,
+      service: "yahoo",
+      secure: false,
+      auth: {
+        user: process.env.YAHOO_USER,
+        pass: process.env.YAHOO_PASS,
+      },
+      debug: false,
+      logger: true,
+    });
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+}
+
+function makePassword(length) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 function genToken(admin) {
   const payload = {
